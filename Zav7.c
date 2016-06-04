1180678a8b8f70e7a3634c0e73dd512e7559e653
Vlad667 <genjas213@gmail.com>
2016-06-04 14:47:25 +0300

    Матрица

---
+++ Zav7.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	
+	int mas[100][100];
+    int i, j, razmer;
+	
+	printf("Vvedite razmer = ");
+	scanf("%d", &razmer);
+	
+	for(i = 0; i < razmer; i++)
+    {
+	for ( j = 0; j < razmer; j++)
+    {
+	if(i >= j)
+    {
+	mas[i][j] = 1;
+	} 
+    else 
+      {
+	mas[i][j] = 0;
+	}
+	}
+	  }
+    
+    for(i = 0; i < razmer; i++)
+      {
+	for ( j = 0; j < razmer; j++)
+      {
+    if(i >= razmer - j)
+	mas[i][j] = 0;
+      }
+	}
+	
+	for(i = 0; i < razmer; i++)
+          {
+	for(j = 0; j < razmer; j++)
+	printf("%3d", mas[i][j]);
+	printf("\n");
+          }
+	system("pause");
+	
+	return 0;
+}
